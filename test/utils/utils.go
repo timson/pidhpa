7c9733cf98f7b17047d0d70e0074e9aba3cde1ae
Timothy <tim.sleptsov@gmail.com>
2025-02-12 17:31:24 +0200

    fix staticcheck

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -22,7 +22,7 @@ import (
 	"os/exec"
 	"strings"
 
-	// lint:ignore ST1001 This is a test file and should be in the test package
+	//lint:ignore ST1001 This is a test file and should be in the test package
 	. "github.com/onsi/ginkgo/v2" //nolint:golint,revive
 )
 
